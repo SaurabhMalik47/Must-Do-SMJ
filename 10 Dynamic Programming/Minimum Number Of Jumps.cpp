9e6586c9b714b39baa6b2cc1545ca86ab381e082
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-18 17:35:08 +0530

    Minimum Jumps to Reach End Of Array [REHHRA]

---
+++ 10 Dynamic Programming/Minimum Number Of Jumps.cpp
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given an array of integers where each element represents the maximum number of steps that can be made forward from that element.
+The task is to find the minimum number of jumps to reach the end of the array (starting from the first element). 
+If an element is 0, then cannot move through that element.
+
+Input:
+11
+1 3 5 8 9 2 6 7 6 8 9
+6
+1 4 3 2 6 7
+
+Output:
+3
+2
+
+/****************************** Time Complexity : O(n^2) and Space Complexity : O(n) ******************************/
