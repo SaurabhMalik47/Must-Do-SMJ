5140a465d33544f5ef4413a17fe1552bc86ac76c
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-19 17:12:37 +0530

    String को  Dictionary Words में तोडा जा सके ?

--- 10 Dynamic Programming/[Imp] Word Break.cpp
+++ 10 Dynamic Programming/[Imp] Word Break.cpp
@@ -2,3 +2,57 @@
 Problem:
 Given an input string and a dictionary of words, find out if the input string can be segmented into a 
 space-separated sequence of dictionary words. See following examples for more details.
+
+Consider the following dictionary : { i, like, sam, sung, samsung, mobile, ice, cream, icecream, man, go, mango}
+
+Input : ilike
+Output : Yes
+The string can be segmented as "i like".
+
+Input : ilikesamsung
+Output : Yes
+The string can be segmented as "i like samsung" or "i like sam sung".
+
+Input:
+12
+i like sam sung samsung mobile ice cream icecream man go mango
+ilike
+12
+i like sam sung samsung mobile ice cream icecream man go mango
+idontlike
+
+Output:
+1
+0
+
+
+/****************************** Time Complexity : O(n) and Space Complexity : O(1) ******************************/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   string s, x;
+   unordered_map<string, int> dict;
+   for(int i=0; i<n; i++)
+   {
+      cin>>x;
+      dict[x]++;
+   }
+   cin>>s;
+   x = "";
+
+   for(int i=0; i<s.length(); i++)
+   {
+      x += s[i];
+      if(dict[x])
+         x = "";
+   }
+         
+   if(x == "")
+      cout<<"1"<<endl;
+   else
+      cout<<"0"<<endl;
+	return 0;
+}
