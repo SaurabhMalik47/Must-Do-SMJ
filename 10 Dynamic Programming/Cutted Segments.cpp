b674456707c2b2383e0be1a962186a41cfc45211
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-19 15:00:39 +0530

    Maximise Cutted Segments

--- 10 Dynamic Programming/Cutted Segments.cpp
+++ 10 Dynamic Programming/Cutted Segments.cpp
@@ -14,4 +14,30 @@ Output:
 4
 2
 
-/****************************** Time Complexity : O(n^2) and Space Complexity : O(n) ******************************/
+Explaination:
+In first test case, total length is 4, and cut lengths are 2, 1 and 1.  We can make maximum 4 segments each of length 1.
+In second test case, we can make two segments of lengths 3 and 2.
+
+/****************************** Time Complexity : O(n) and Space Complexity : O(n) ******************************/
+int MaximumCuttedSegments(int n, int x, int y, int z)
+{
+    int dp[n+1];
+    dp[0] = 0;
+    int i;
+    for(i=1; i<=n; i++)
+      dp[i] = -1;
+            
+    for(i=0; i<n; i++)
+    {
+        if(dp[i] != -1)
+        {
+            if(i+x <= n)
+				       dp[i+x] = max(dp[i]+1, dp[i+x]);
+		        if(i+y <= n)
+		           dp[i+y] = max(dp[i]+1, dp[i+y]);
+		        if(i+z <= n)    
+		           dp[i+z] = max(dp[i]+1, dp[i+z]);
+        }
+    }
+    return dp[n];
+}
