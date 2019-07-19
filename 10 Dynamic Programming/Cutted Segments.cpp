6d1479ad2073e0a84e09ee7fee33bacb05624c19
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-19 13:34:50 +0530

    Maximise Cutted Segments

---
+++ 10 Dynamic Programming/Cutted Segments.cpp
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given an integer N denoting the Length of a line segment. You need to cut the line segment in such a way that the 
+cut length of a line segment each time is integer either x, y or z.and after performing all cutting operation
+the total number of cutted segments must be maximum. 
+
+Input:
+4         // N
+2 1 1     // x, y, z
+5
+5 3 2
+
+Output:
+4
+2
+
+/****************************** Time Complexity : O(n^2) and Space Complexity : O(n) ******************************/
