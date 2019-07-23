627480595704f34739c79372fcb3d64df8648d3d
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-23 11:41:41 +0530

    Find First Set Bit (Rightmost Set Bit)

---
+++ 13 Bit Magic/Find First Set Bit.cpp
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given an integer an N. The task is to print the position of first set bit found from right side in the binary representation of the number.
+
+Input:
+18
+12
+
+Output:
+2
+3
+
+Explanation:
+Testcase 1 - Binary representation of the 18 is 010010, the first set bit from the right side is at position 2.
+
+
+/****************************** Time Complexity : O(n^2) and Space Complexity : O(n) ******************************/
