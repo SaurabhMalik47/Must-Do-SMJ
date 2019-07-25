addfb344169308b7031cd151b8795e11fab88982
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-25 11:50:31 +0530

    [REHHRA]Rearrange Array In Maximum - Minimum Form

---
+++ 01 Array/Rearrange Array Alternately - O(1) Space.cpp
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given a sorted array of positive integers. Your task is to rearrange  the array elements alternatively 
+i.e first element should be max value, second should be min value, third should be second max, fourth should be second min and so on...
+
+Note: O(1) extra space is allowed. Also, try to modify the input array as required.
+
+Input:
+6
+1 2 3 4 5 6
+11 
+10 20 30 40 50 60 70 80 90 100 110
+
+Output:
+6 1 5 2 4 3
+110 10 100 20 90 30 80 40 70 50 60
+
