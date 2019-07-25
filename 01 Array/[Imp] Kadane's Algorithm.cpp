ffb9487e923b123bf377bae2020c3eb0c2692366
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-07-25 11:08:59 +0530

    Largest Sum Contiguous Subarray

--- 01 Array/[Imp] Kadane's Algorithm.cpp
+++ 01 Array/[Imp] Kadane's Algorithm.cpp
@@ -17,17 +17,17 @@ Output:
 
 int KadaneAlgorithm(int a[], int n)
 {
-	 int current_sum = 0, max_sum = a[0];
+   int current_sum = 0, max_sum = a[0];
 
-	 for(int i=0; i<n; i++)
-	 {
-	    current_sum += a[i];
+   for(int i=0; i<n; i++)
+   {
+      current_sum += a[i];
 	    
-	    if(current_sum >= max_sum)
-	       max_sum = current_sum;
+      if(current_sum >= max_sum)
+	 max_sum = current_sum;
 	       
-	    if(current_sum < 0)
-	       current_sum = 0;
-	 }
-	 return max_sum;
+      if(current_sum < 0)
+	 current_sum = 0;
+   }
+   return max_sum;
 }
