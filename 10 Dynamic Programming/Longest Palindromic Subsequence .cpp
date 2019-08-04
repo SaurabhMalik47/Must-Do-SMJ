fc922335c625b5b0ea2f0d1e52662499da67e737
Saurabh Malik <52415729+SaurabhMalik47@users.noreply.github.com>
2019-08-04 18:51:02 +0530

    LPS

---
+++ 10 Dynamic Programming/Longest Palindromic Subsequence .cpp
@@ -0,0 +1,18 @@
+/*
+Problem:
+Given a sequence, find the length of the longest palindromic subsequence in it.
+
+Input:
+GEEKSFORGEEKS
+BBABCBCAB
+
+Output:
+5
+7
+
+Explaination:
+Testcase 1: "EEKEE",  "EESEE", etc. is the Longest Palindromic Subsequence with Length 5.
+Testcase 2: “BABCBAB” is the Longest Palindromic Subsequence with Length 7.
+
+/****************************** Time Complexity : O(n) and Space Complexity : O(1) ******************************/
+
